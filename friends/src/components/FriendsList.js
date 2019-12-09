c78bee3d166fa07867bf1b8bae3b1e7931c0fe09
Buddy Agyin <buddyagyin@Buddys-MacBook-Pro.local>
2019-12-09 14:59:22 -0700

    add edit friend, delete friend functionality

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, previousState } from 'react'
 import { axiosWithAuth } from './axiosWithAuth'
-import { Card } from 'semantic-ui-react'
+import { Card, Button, Icon } from 'semantic-ui-react'
 import FriendsForm from './FriendsForm'
+import FriendModal from './FriendModal'
 
 const FriendsList = () => {
     const [friends, setFriends] = useState([])
@@ -30,6 +31,25 @@ const FriendsList = () => {
         setFriend({name: '', age: '', email: ''})
     }
 
+    const deleteFriend = id => {
+        axiosWithAuth()
+            .delete(`/friends/${id}`)
+            .then(res => {
+                console.log('delete: ' + res)
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+        axiosWithAuth()
+            .get('/friends')
+            .then(res => {
+                setFriends(res.data)
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    }
+
     useEffect(() => {
         const getData = () => {
             axiosWithAuth()
@@ -43,16 +63,34 @@ const FriendsList = () => {
         getData()
     }, [])
 
+
+
     return(
         <div>
             <FriendsForm friend={friend} handleChange={handleChange} addFriend={addFriend} />
-            {friends.map(friend => 
-                <Card key={friend.id}>
-                    <Card.Content>
-                        <Card.Header>{friend.name}</Card.Header>
-                    </Card.Content>
-                </Card>
-                )}
+            <div className='card-container'>
+                <Card.Group centered stackable itemsPerRow={3}>
+                    {friends.map(friend => 
+                        <Card key={friend.id}>
+                            <Card.Content textAlign='center'>
+                                <Card.Header>{friend.name}</Card.Header>
+                                <Card.Description>
+                                    Age: {friend.age}<br/>
+                                    Email: {friend.email}
+                                </Card.Description>
+                            </Card.Content>
+                            <Card.Content  extra>
+                                <Button.Group floated='right'>
+                                    <FriendModal friend={friend.id} setFriends={setFriends} handleChange={handleChange} />
+                                    <Button onClick={() => deleteFriend(friend.id)}>
+                                        <Icon name='trash' />
+                                    </Button>
+                                </Button.Group>
+                            </Card.Content>
+                        </Card>
+                        )}
+                </Card.Group>
+            </div>
         </div>
     )
 }
